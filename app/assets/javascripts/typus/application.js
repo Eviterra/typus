e3d5bccfc3d95dc21df94a665e07c2e49d6d1a2b
codesnik <aronaxis@gmail.com>
2012-01-21 00:13:56 +0400

    Added jquery-ui and daterangepicker.jQuery.

--- app/assets/javascripts/typus/application.js
+++ app/assets/javascripts/typus/application.js
@@ -3,6 +3,9 @@
 //= require jquery.fancybox-1.3.4.pack
 //= require jquery.searchField
 //= require js/jquery.formalize.min
+//= require jquery-ui
+//= require daterangepicker/date.js
+//= require daterangepicker/daterangepicker.jQuery
 //= require typus/jquery.application
 //= require typus/custom
 //= require chosen.jquery
